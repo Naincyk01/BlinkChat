8f23ff42fc9846461078da99a9c63ed1802853a9
Naincy <naincyk15042004@gmail.com>
2024-07-18 12:30:05 +0530

    Updated the group routes according to new methods added to group controller

--- backend/src/routes/group.routes.js
+++ backend/src/routes/group.routes.js
@@ -5,6 +5,8 @@ import {
   addParticipants,
   removeParticipant,
   deleteGroup,
+  updateGroup,
+  leaveGroup
 } from '../controllers/group.controller.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js'; 
 
@@ -21,8 +23,12 @@ router.get('/', getConversations);
 
 router.put('/:groupId/participants', addParticipants);
 
-router.delete('/:groupId/participantId', removeParticipant);
+router.delete('/:groupId/participants/:participantId', removeParticipant);
 
 router.delete('/:groupId', deleteGroup);
+
+router.put('/:groupId', updateGroup);
+
+router.delete('/:groupId/leave', leaveGroup);
 
 export default router;
